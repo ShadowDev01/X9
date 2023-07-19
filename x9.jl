18119ab0175d919de7701c891da4825295e53240
miti <miti@gmail.com>
2023-07-19 10:35:06 +0330

    minor change

--- x9.jl
+++ x9.jl
@@ -67,7 +67,7 @@ end
 res = String[]
 
 function parameters(url::String)
-    reg = r"\=([\w\-\%\.\:\~\,\/]+)?"   # extract the value of default parameters in url
+    reg = r"\=([\w\-\%\.\:\~\,\"\'\<\>\=\(\)\`\{\}\$\+\/\;]+)?"   # extract the value of default parameters in url
     return [i.captures[1] for i in eachmatch(reg, "?$url")]
 end
 
